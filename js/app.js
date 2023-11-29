8da15bfa183a218fe2a92c47f8da05d521ed7e80
Axel Pereyra <axelpereyra067@gmail.com>
2023-11-29 17:20:27 -0300

    tercera pre entrega finalizada

--- js/app.js
+++ js/app.js
@@ -1,44 +1,55 @@
-/* ingresar peso y altura */
-let height = 1.80 /* en metros */
-let weight = 55 /* en kilos */
+// Obtener elementos del DOM
+const weightInput = document.getElementById('weightInput');
+const heightInput = document.getElementById('heightInput');
+const calculateButton = document.getElementById('calculateButton');
+const resultContainer = document.getElementById('resultContainer');
 
-let users = []
+// Cargar usuarios desde localStorage o inicializar array vacio
+let users = JSON.parse(localStorage.getItem('users')) || [];
 
+// Función para validar datos
+const dataValidation = (height, weight) => {
+    return !isNaN(weight) && !isNaN(height) && weight > 0 && height > 0;
+}
 
-/* validacion de datos de peso y altura */
-const DataValidation = (height, weight)  => {
-    while (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
-        console.log("Please enter a valid weight or a valid weight")
-        return;
+// Funcion para calcular IMC y mostrar resultado
+const calculateIMC = () => {
+    const height = parseFloat(heightInput.value);
+    const weight = parseFloat(weightInput.value);
+
+    if (dataValidation(height, weight)) {
+        const imc = weight / (height * height);
+
+        console.log(`IMC: ${imc.toFixed(2)}`);
+
+        let resultText = '';
+        let resultColor = '';
+
+        if (imc < 18.5) {
+            resultText = "Low weight";
+            resultColor = 'red';
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            resultText = "Normal weight";
+            resultColor = 'green';
+        } else if (imc >= 25 && imc <= 29.9) {
+            resultText = "Overweight";
+            resultColor = 'yellow';
+        } else {
+            resultText = "Obesity";
+            resultColor = 'red';
+        }
+
+        // Mostrar resultado en pantalla con color correspondiente
+        resultContainer.innerHTML = `<p style="color: ${resultColor};">${resultText}</p>`;
+
+        // Guardar usuario en el array y en localStorage
+        const user = { weight, height, imc, resultText, resultColor };
+        users.push(user);
+        localStorage.setItem('users', JSON.stringify(users));
+    } else {
+        console.log("Please enter valid weight and height");
     }
 }
-const user = {
-  weight: parseFloat(weight),
-  height: parseFloat(height),
-}
-users.push(user)
-/* calcular el indice de masa corporal */
-const IMC = (height, weight) => {
-    /* validar datos */
-    DataValidation(height, weight);
-    /* calcular indice de masa corporal */
-    let imc = weight / (height * height)
 
-    /* imprimir el indice de masa corporal redondeado en el caso que sea un numero */
-    if (!isNaN(imc)){
-        console.log(imc.toFixed(2))
-        if (imc < 18.5){
-            console.log("low weight")
-        }
-        else if(imc >= 18.5 && imc <= 24.9){
-            console.log("normal weight")
-        }
-        else if(imc >= 25 && imc <= 29.9) {
-            console.log("overweight")
-        }
-        else{
-            console.log("obesity")
-        }
-    }
-}
-IMC(height, weight);
+// Agregar evento al boton de calcular IMC
+calculateButton.addEventListener('click', calculateIMC);
